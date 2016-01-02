ce9ad5937ea103e063a7138f64dfe76dd74bd0fc
F4b1x <fabferaud@orange.fr>
2016-01-02 16:13:57 +0100

    Update TexteEnrichis.cpp

--- TexteEnrichis.cpp
+++ TexteEnrichis.cpp
@@ -31,32 +31,9 @@ TexteEnrichis::TexteEnrichis(char* arg, size_t n): nbLigne(0), TabStr(0), OldCol
     }
 }
 
-TexteEnrichis::TexteEnrichis(const std::string arg, size_t n): nbLigne(0), TabStr(0), OldColor(0), NomFichier(NULL), Texte(""), _n(0) 
+TexteEnrichis::TexteEnrichis(const std::string arg, size_t n): nbLigne(0), TabStr(0), OldColor(0), NomFichier(NULL), Texte(arg), _n(0) 
 {
-    std::string ligne="";
-
     std::cout << "L'argument est un string" << std::endl;
-    
-    int i=0;
-    std::string phrase="";
-    while (i != arg.size())
-    {
-         
-        if (arg[i] == '\n')
-        {
-            TabStr.push_back(phrase);
-            nbLigne++;
-            phrase="";
-        }
-        else
-        {
-            phrase += arg[i];
-        }
-        std::cout<<phrase<<";"<<std::endl;;
-        i++;
-    }
-    TabStr.push_back(phrase);
-    nbLigne++;
 }
 
 std::vector<std::string> TexteEnrichis::getTab()
@@ -217,44 +194,21 @@ void TexteEnrichis::TraitementBaliseCouleur(std::string& phrase, std::string& mo
 
 
 
-void TexteEnrichis::TraitementBalise()
+void TexteEnrichis::TraitementTexte()
 {
+
+    std::string ligne="";
+    std::string phrase="";
+    std::string MotDuTexte="";
+    bool VerbatimActif = false;
+
     if (NomFichier != NULL) 
     {
-        std::string ligne="";
-        std::string phrase="";
-        bool VerbatimActif = false;
-    
-    
         std::ifstream fich(NomFichier);
     
         if (!fich.is_open())
         {
-            std::cout << "L'argument n'est pas un fichier" << std::endl;
-            
-            int i=0;
-            
-            
-            while (Texte[i] != '\0')
-            {
-                std::cout<<Texte[i]<<std::endl;
-                
-                if (Texte[i] == '\n')
-                {
-                    TabStr.push_back(phrase);
-                    nbLigne++;
-                    phrase="";
-                }
-                else
-                {
-                    phrase += Texte[i];
-                }
-                std::cout<<phrase<<";"<<std::endl;;
-                i++;
-            }
-            TabStr.push_back(phrase);
-            nbLigne++;
-    
+            std::cout << "Le fichier ne s'est pas ouvert" << std::endl;
         }
         else
         {
@@ -263,137 +217,19 @@ void TexteEnrichis::TraitementBalise()
             while (!fich.eof()) 
             {
                 getline(fich, ligne);
-                int i=0;
-                bool Interrupt=false;
-                while ( i<ligne.size()+1)
-                {
-                    
-                    if ( ligne[i] == '\0' )
-                    {
-                        
-                        if (!Interrupt)
-                        {
-                            TabStr.push_back(phrase);
-                            nbLigne+=1;
-                        }
-                        phrase="";
-                        i++;
-                        Interrupt=false;
-    
-                        
-                    }
-                    else
-                    {
-                        Interrupt=false;
-                        if ( ligne[i] == '<')
-                        {
-                            bool BaliseFermante = false;
-                            std::string motBalise="";
-    
-                            if ( ligne[i+1] == '/')
-                            {
-                                BaliseFermante = true;
-                            }
-    
-                            motBalise+=ligne[i];
-                            
-                            do
-                            {
-                                i++;
-                                motBalise+=ligne[i];
-                                
-    
-                            } while (ligne[i] != '>');
-    
-    
-                            std::cout<<"motBalise :"<<motBalise<<std::endl;
 
-                            
-    
-                            if (motBalise == "<verbatim>")
-                            {
-                                VerbatimActif = true;
-                                phrase+="";
-                            }
-    
-                            else if ( motBalise == "</verbatim>")
-                            {
-                                VerbatimActif = false;
-                                phrase+="";
-                            }
-    
-                            else if ( !BaliseFermante && !VerbatimActif)
-                            {
-                                TraitementBaliseOuvrante(phrase, motBalise, Interrupt);
-                                
-                            }
-    
-                            else if (BaliseFermante && !VerbatimActif)
-                            {
-                                if(motBalise == "</b>")
-                                {
-                                    phrase+="\033[21m";
-                                }
-                                else if (motBalise == "</i>")
-                                {
-                                    phrase+="\033[23m";
-                                }
-                                else if (motBalise == "</u>")
-                                {
-                                    phrase+="\033[24m";
-                                }
-                                else if (motBalise == "</blink>")
-                                {
-                                    phrase+="\033[25m";
-                                }
-                                else if (motBalise == "</reverse>")
-                                {
-                                    phrase+="\033[27m";
-                                }
-                                else if (motBalise == "</p>")
-                                {
-                                    //ActionsSiValeurN;
-                                }
-                                else if (motBalise == "</color>")
-                                {
-                                    size_t taille = OldColor.size();
-                                    if (taille == 1)
-                                    {
-                                        phrase+="\033[0m";
-                                    }
-                                    else if (taille > 1)
-                                    {
-                                        phrase+=OldColor[taille-2];
-                                        OldColor.pop_back();
-                                    }
-                                }
-                                else
-                                {
-                                    phrase+=motBalise;
-                                }
-                                
-                            }
-                            else if (VerbatimActif)
-                            {
-                                phrase+=motBalise;
-                            }
-    
-                            i++;
-                        }
-                        else
-                        {
-                            phrase+=ligne[i];
-                            i++;
-                        }
-                        //std::cout<<"Etat Interrupt"<<((Interrupt)?"1":"0")<<std::endl;
-                    }
-                    
-                }
+                AnalyseString(ligne, MotDuTexte, phrase, VerbatimActif);
                 
             }
             
             fich.close();
         }
+    }
+    else
+    {
+        std::cout<<"L'argument n'est pas un fichier"<<std::endl;
+
+        AnalyseString(Texte, MotDuTexte, phrase, VerbatimActif);
     }
 }
 
@@ -450,17 +286,149 @@ void TexteEnrichis::TraitementBaliseOuvrante(std::string& phrase, std::string& m
 }
 
 
-/*void TexteEnrichis::TraitementBaliseFermante(std::string& phrase, std::string& motBalise)
+void TexteEnrichis::TraitementBaliseFermante(std::string& phrase, std::string& motBalise)
 {
-    //
-}*/
+    if(motBalise == "</b>")
+    {
+        phrase+="\033[21m";
+    }
+    else if (motBalise == "</i>")
+    {
+        phrase+="\033[23m";
+    }
+    else if (motBalise == "</u>")
+    {
+        phrase+="\033[24m";
+    }
+    else if (motBalise == "</blink>")
+    {
+        phrase+="\033[25m";
+    }
+    else if (motBalise == "</reverse>")
+    {
+        phrase+="\033[27m";
+    }
+    else if (motBalise == "</p>")
+    {
+        //ActionsSiValeurN;
+    }
+    else if (motBalise == "</color>")
+    {
+        size_t taille = OldColor.size();
+        if (taille == 1)
+        {
+            phrase+="\033[0m";
+        }
+        else if (taille > 1)
+        {
+            phrase+=OldColor[taille-2];
+            OldColor.pop_back();
+        }
+    }
+    else
+    {
+        phrase+=motBalise;
+    }
+}
+
+void TexteEnrichis::AnalyseString(std::string& ligne, std::string& MotDuTexte, std::string& phrase, bool& VerbatimActif)
+{
 
 
+
+    int i=0;
+    bool Interrupt=false;
+    while ( i<ligne.size()+1)
+    {
+        if ( ligne[i] == '\0' )
+        {
+            
+            if (!Interrupt)
+            {
+                TabStr.push_back(phrase);
+                nbLigne+=1;
+                
+            }
+            phrase="";
+            i++;
+            Interrupt=false;
+
+            
+        }
+        else
+        {
+            Interrupt=false;
+            if ( ligne[i] == '<')
+            {
+                bool BaliseFermante = false;
+                std::string motBalise="";
+
+                if ( ligne[i+1] == '/')
+                {
+                    BaliseFermante = true;
+                }
+
+                motBalise+=ligne[i];
+                
+                do
+                {
+                    i++;
+                    motBalise+=ligne[i];
+                    
+
+                } while (ligne[i] != '>');
+
+
+                std::cout<<"motBalise :"<<motBalise<<std::endl;
+
+                
+
+                if (motBalise == "<verbatim>")
+                {
+                    VerbatimActif = true;
+                    phrase+="";
+                }
+
+                else if ( motBalise == "</verbatim>")
+                {
+                    VerbatimActif = false;
+                    phrase+="";
+                }
+
+                else if ( !BaliseFermante && !VerbatimActif)
+                {
+                    TraitementBaliseOuvrante(phrase, motBalise, Interrupt);
+                    
+                }
+
+                else if (BaliseFermante && !VerbatimActif)
+                {
+                    TraitementBaliseFermante(phrase, motBalise);
+                    
+                }
+                else if (VerbatimActif)
+                {
+                    phrase+=motBalise;
+                }
+
+                i++;
+            }
+            else
+            {
+                phrase+=ligne[i];
+                i++;
+            }
+            
+        }
+        
+    }
+
+}
 
 
 void affiche(std::ostream& os, TexteEnrichis& T)
 {
-    for (size_t i=0;i<T.getNbLigne()-1;i++)
+    for (size_t i=0;i<T.getNbLigne();i++)
         os<<T.getTab().at(i)<<std::endl;
-    os<<T.getNbLigne()-1<<std::endl;
+    os<<T.getNbLigne()<<std::endl;
 }
